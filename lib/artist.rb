220618a15697b08c8074c2b014c09b5dc3fb69d2
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-03 19:21:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,11 +4,11 @@ class Artist
 
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-
+  extend Findable 
 
   attr_accessor :name
   attr_reader :songs
-  
+
 
   @@artists = []
 
