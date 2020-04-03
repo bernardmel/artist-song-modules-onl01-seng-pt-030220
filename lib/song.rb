c5d1092afe7a46276e2c4cf3ff4c4dc0b1b5b853
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-03 19:28:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,14 +7,14 @@ class Song
   extend Findable
   include Paramable
 
-  attr_accessor :name
-  attr_reader :artist
+  attr_accessor :name, :artist
+  # attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
@@ -32,9 +32,9 @@ class Song
   #   self.all.count
   # end
 
-  def artist=(artist)
-    @artist = artist
-  end
+  # def artist=(artist)
+  #   @artist = artist
+  # end
 
   # def to_param
   #   name.downcase.gsub(' ', '-')
