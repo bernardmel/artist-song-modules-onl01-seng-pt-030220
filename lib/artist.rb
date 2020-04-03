e3f987e03ceb5f8a7c94ae3674656ccad32752e4
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-03 19:24:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  extend Findable 
+  extend Findable
 
   attr_accessor :name
   attr_reader :songs
@@ -42,7 +42,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
