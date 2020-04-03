ad9766b63da87dafe26ad9bd56c83f414269929c
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-04-03 19:00:31 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,3 +1,21 @@
 module Momorable
 
+    module ClassMethods
+
+      def reset_all
+        self.all.clear
+      end
+
+      def count
+        self.all.count
+      end
+
+    end
+
+    module InstanceMethods
+
+      def initialize
+        self.class.all << self
+      end
+    end
 end
